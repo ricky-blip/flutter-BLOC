428ef38326492c12bd61c774aeb99f51010bfa99
ricky-blip <ricky.rinaldy77@gmail.com>
2023-01-16 20:26:53 +0700

    update cubit observer

--- lib/roadmap/0.intro/2.cubit_observer.dart
+++ lib/roadmap/0.intro/2.cubit_observer.dart
@@ -2,21 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubitObserver extends Cubit<int> {
-  CounterCubitObserver() : super(0);
+  CounterCubitObserver({this.customData = 0}) : super(customData);
+
+  int customData;
+
+  //SECTION - Observer
+  int? currentNumber;
+  int? nextNumber;
 
   void min() {
-    if (state > 0) {
+    if (state > customData) {
       emit(state - 1);
     }
   }
 
   void plus() => emit(state + 1);
+
+  //NOTE - we can observe data : onChange, onError etc.
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+    currentNumber = change.currentState;
+    nextNumber = change.nextState;
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    print(error);
+  }
 }
 
 class CubitObserver extends StatelessWidget {
   CubitObserver({super.key});
 
-  CounterCubitObserver myCount = CounterCubitObserver();
+  CounterCubitObserver myCount = CounterCubitObserver(customData: 77);
 
   @override
   Widget build(BuildContext context) {
@@ -31,16 +52,38 @@ class CubitObserver extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           StreamBuilder(
+            initialData: myCount.customData,
             stream: myCount.stream,
             builder: (context, snapshot) {
               return Center(
-                child: Text(
-                  "${snapshot.data}",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    color: Colors.deepPurpleAccent,
-                    fontWeight: FontWeight.bold,
-                  ),
+                child: Column(
+                  children: [
+                    Text(
+                      "${snapshot.data}",
+                      style: const TextStyle(
+                        fontSize: 50,
+                        color: Colors.deepPurpleAccent,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      "Current : ${myCount.currentNumber}",
+                      style: const TextStyle(
+                        fontSize: 30,
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Next : ${myCount.nextNumber}",
+                      style: const TextStyle(
+                        fontSize: 30,
+                        color: Colors.purple,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               );
             },
