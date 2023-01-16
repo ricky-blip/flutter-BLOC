b9748417e92aa85d9c17d6e8192571aa65c4d6ea
ricky-blip <ricky.rinaldy77@gmail.com>
2023-01-16 20:02:03 +0700

    update create cubit observer

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -56,7 +56,7 @@ class HomePage extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const CubitObserver(),
+                        builder: (context) => CubitObserver(),
                       ),
                     );
                   },
--- lib/roadmap/0.intro/2.cubit_observer.dart
+++ lib/roadmap/0.intro/2.cubit_observer.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubitObserver extends Cubit<int> {
+  CounterCubitObserver() : super(0);
+
+  void min() {
+    if (state > 0) {
+      emit(state - 1);
+    }
+  }
+
+  void plus() => emit(state + 1);
+}
 
 class CubitObserver extends StatelessWidget {
-  const CubitObserver({super.key});
+  CubitObserver({super.key});
+
+  CounterCubitObserver myCount = CounterCubitObserver();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.deepPurpleAccent,
+        centerTitle: true,
+        title: const Text("Cubit Observer"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StreamBuilder(
+            stream: myCount.stream,
+            builder: (context, snapshot) {
+              return Center(
+                child: Text(
+                  "${snapshot.data}",
+                  style: const TextStyle(
+                    fontSize: 30,
+                    color: Colors.deepPurpleAccent,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              );
+            },
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  myCount.min();
+                },
+                icon: const Icon(Icons.remove),
+              ),
+              IconButton(
+                onPressed: () {
+                  myCount.plus();
+                },
+                icon: const Icon(Icons.add),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
